356591e457adfa284a21f07d555372b071d2b64e
safa fedia <safafedia@gmail.com>
2024-02-21 20:38:13 +0100

    Description des modifications apportées

---
+++ Controllers/userController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pfe5.Controllers
+{
+    public class userController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Service()
+        {
+            return View();
+        }
+        public IActionResult Rays()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+      
+    }
+}
